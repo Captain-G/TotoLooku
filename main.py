bc6f1dfb0feabe36b4aae11e036230e100dea1a8
Captian-G <poppinpites7@gmail.com>
2024-03-06 12:24:17 +0300

    Updated Homepage

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from flet import *
 def main(page: Page):
     page.update()
 
+    BG = "#041955"
+    FWG = "#97b4ff"
+    FG = "#3450a1"
+    PINK = "#eb06ff"
+
     def change_tab(e):
         # get index tab
         my_index = e.control.selected_index
@@ -13,7 +18,7 @@ def main(page: Page):
         page.update()
 
     page.navigation_bar = NavigationBar(
-        bgcolor="blue",
+        bgcolor=FG,
         selected_index=0,
         on_change=change_tab,
         destinations=[
@@ -22,13 +27,111 @@ def main(page: Page):
             NavigationDestination(icon="settings"),
         ]
     )
-    tab_1 = Text("Tab 1", size=30, visible=True)
+    categories_card = Row(
+        scroll="auto"
+    )
+    categories = ["Sweatshirts", "Sweaters", "Jeans", "Dresses", "Jackets", "Shoes", "Socks", "Boxers"]
+    for i, category in enumerate(categories):
+        sold_pieces = 100 - i * 5
+        categories_card.controls.append(
+            Container(
+                bgcolor=FG, height=110, width=170, border_radius=20, padding=15,
+                content=Column(
+                    controls=[
+                        Text(category, size=20),
+                        Text(f"Sold {sold_pieces} Pcs", size=14, italic=True),
+                        Container(
+                            width=160,
+                            height=5,
+                            bgcolor="white12",
+                            border_radius=18,
+                            padding=padding.only(right=i * 15),
+                            content=Container(
+                                bgcolor=PINK,
+                            )
+                        )
+                    ]
+                )
+            )
+        )
+    # tab_1 = Text("Tab 1", size=30, visible=True)
+    tab_1 = Container(
+        visible=True,
+        content=Column(
+            controls=[
+                Row(
+                    alignment="spaceBetween",
+                    controls=[
+                        Container(
+                            # on_click=lambda e: shrink(e),
+                            content=Icon(icons.MENU)
+                        ),
+                        Row(
+                            controls=[
+                                Icon(icons.SEARCH),
+                                Icon(icons.NOTIFICATIONS_OUTLINED)
+                            ]
+                        )
+                    ]
+                ),
+                Container(height=5),
+                Text(
+                    value="What\'s up Gachuki!"
+                ),
+                Container(height=5),
+                Text(
+                    value="CATEGORIES"
+                ),
+                Container(
+                    padding=padding.only(top=10, bottom=20, ),
+                    content=categories_card
+                ),
+                Text("STOCKS"),
+                Container(
+                    DataTable(
+                        columns=[
+                            DataColumn(Text("Item")),
+                            DataColumn(Text("Start"), numeric=True),
+                            DataColumn(Text("Sold"), numeric=True),
+                            DataColumn(Text("Remaining"), numeric=True),
+                        ],
+                        rows=[
+                            DataRow(
+                                cells=[
+                                    DataCell(Text("Sweatshirts")),
+                                    DataCell(Text("155")),
+                                    DataCell(Text("100")),
+                                    DataCell(Text("55")),
+                                ],
+                            ),
+                            DataRow(
+                                cells=[
+                                    DataCell(Text("Sweaters")),
+                                    DataCell(Text("102")),
+                                    DataCell(Text("20")),
+                                    DataCell(Text("82")),
+                                ],
+                            ),
+                            DataRow(
+                                cells=[
+                                    DataCell(Text("Jeans")),
+                                    DataCell(Text("90")),
+                                    DataCell(Text("40")),
+                                    DataCell(Text("50")),
+                                ],
+                            ),
+                        ],
+                    ),
+
+                )
+            ]
+        )
+    )
     tab_2 = Text("Tab 2", size=30, visible=False)
     tab_3 = Text("Tab 3", size=30, visible=False)
 
     page.add(
         Container(
-            margin=margin.only(top=page.window_height / 2, left=50),
             content=Column(
                 [
                     tab_1, tab_2, tab_3
